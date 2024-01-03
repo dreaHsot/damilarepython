3326b9d946290d1cca7c6f22b577ea75d3de1715
damilare <damilaresotunde@yahoo.com>
2024-01-04 00:11:56 +0100

    modified the code for scalabitity; use of function

--- mancity_game/main_code.py
+++ mancity_game/main_code.py
@@ -3,25 +3,17 @@ import players_DB
 import game_art
 import random
 
-os.system('clear')
-
 print(game_art.manchester_city)
 continue_flag = True
 p_point = 0
 
-#print("Choose level")
-while continue_flag:
-    player1 = random.choice(players_DB.player_data)
-    player2 = random.choice(players_DB.player_data)
-    A = player1["name"]
-    B = player2["name"]
-
-    Aposition = player1["position"]
-    Bposition = player2["position"]
-
-    print(f"whose jersey number is greater?\n\nA:{A} ({Aposition})\n{game_art.vs}\nB:{B} ({Bposition})\n")
+def jersey_number_compare():
+    global p_point
+    global continue_flag
+       
+    print(f"whose jersey number is greater?\n\nA:{Aname} ({Aposition})\n{game_art.vs}\nB:{Bname} ({Bposition})\n")
     
-    if player1["Jersey Number"] > player2["Jersey Number"]:
+    if Ajersey > Bjersey:
         answer = 'A'
     else:
         answer = 'B'
@@ -30,7 +22,27 @@ while continue_flag:
 
     if p_answer == answer:
         p_point += 1
-        print(f"That's correct; you have {p_point} points\n\n")
+        print(f"That's correct\n\n")
     else:
-        print(f"wrong!!! GAME OVER\n\nYou have {p_point} points\n")
+        print(f"wrong!!! GAME OVER\n\nFINAL SCORE = {p_point} points\n")
         continue_flag = False
+
+os.system('clear')
+
+while continue_flag:
+    player1 = random.choice(players_DB.player_data)
+    player2 = random.choice(players_DB.player_data)
+
+    Aname = player1["name"]
+    Bname = player2["name"]
+
+    Aposition = player1["position"]
+    Bposition = player2["position"]
+
+    #Acountry = player1["country"]
+    #Bcountry = player2["country"]
+
+    Ajersey = player1["Jersey Number"]
+    Bjersey = player2["Jersey Number"]
+
+    jersey_number_compare()
